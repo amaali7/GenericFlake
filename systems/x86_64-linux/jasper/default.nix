bc7eeba8d78fb6e39a5933de874a7605bff88e5e
Abdallah Ali <amaali1991@gmail.com>
2023-05-27 02:27:02 +0300

    changeeeeeeeeeed 4

--- systems/x86_64-linux/jasper/default.nix
+++ systems/x86_64-linux/jasper/default.nix
@@ -7,9 +7,7 @@
 with lib;
 with lib.internal; {
   imports = [./hardware.nix];
-  boot.loader.grub = enabled;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
+
   x-next = {
     archetypes = {
       workstation = enabled;
